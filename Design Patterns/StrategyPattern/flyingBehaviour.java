48eecdc48905c82a61bff0fb127743dcd29f3b9e
Adwaith Jayasankar <kichu31084@gmail.com>
2024-12-24 18:44:59 +0000

    ignore

--- Design Patterns/StrategyPattern/flyingBehaviour.java
+++
@@ -1,5 +0,0 @@
-package StrategyPattern;
-
-public interface flyingBehaviour {
-    void fly();
-}
--- Design Patterns/StrategyPattern/jetFlying.java
+++
@@ -1,8 +0,0 @@
-package StrategyPattern;
-
-
-public class jetFlying implements flyingBehaviour{
-    public void fly(){
-        System.out.println("The bird has activated jet flying mode");
-    }
-}
