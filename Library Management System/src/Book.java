a2581a205a9744a8279e42cbcd75f2c1598e72d4
Adwaith Jayasankar <kichu31084@gmail.com>
2025-08-05 18:22:33 +0530

    Update: Started LMS, finished Book class

---
+++ Library Management System/src/Book.java
@@ -0,0 +1,55 @@
+public class Book {
+    private String bookId;
+    private String title;
+    private String author;
+    private Boolean isAvailable;
+    private Member borrowedBy;
+
+    public Book(String bookId, String title, String author){
+        this.bookId = bookId;
+        this.author = author;
+        this.title = title;
+        this.isAvailable = true;
+        this.borrowedBy = null;
+    }
+
+
+    //Define the getters
+    public String getId(){
+        return bookId;
+    }
+
+    public String getBookTitle(){
+        return title;
+    }
+
+    public String getBookAuthor(){
+        return author;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public Member getBorrowedBy(){
+        return borrowedBy;
+    }
+
+    //Mark the book as borrowed
+    public void markAsBorrowed(String bookId, Member member){
+        this.isAvailable = false;
+        this.borrowedBy = member;
+    }
+
+    //Mark the book as returned
+    public void markAsReturned(String bookId, Member member){
+        this.isAvailable = true;
+        this.borrowedBy = null;
+    }
+
+    //An Override method to print the book details in a specific format
+    @Override
+    public String toString(){
+        return "[" + bookId + "]" + title + "by" + author; 
+    }
+}
