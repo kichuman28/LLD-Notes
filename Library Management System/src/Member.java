80ad9d86c1df248b7b7b1d164484e2f48fe68c4b
Adwaith Jayasankar <kichu31084@gmail.com>
2025-08-05 18:38:45 +0530

    Update: Implemented code for Member class

--- Library Management System/src/Member.java
+++ Library Management System/src/Member.java
@@ -1,3 +1,40 @@
+import java.util.ArrayList;
+
 public class Member {
-    
+    private String name;
+    private int memberId;
+    private ArrayList<Book> borrowedBooks;
+
+    public Member(String name, int memberId){
+        this.name = name;
+        this.memberId = memberId;
+        this.borrowedBooks = new ArrayList<>(); //Using array list here like a vector. Infinite pushes. List means I should initialise a size, which is not possible.
+    }
+
+    //Getters
+    public String getMemberName(){
+        return this.name;
+    }
+
+    public int getMemberId(){
+        return this.memberId;
+    }
+
+    public ArrayList<Book> getBorrowedBooks(){
+        return this.borrowedBooks;
+    }
+
+    //Function for adding a book
+    public void addBorrowedBook(Book book){
+        borrowedBooks.add(book);
+    }
+
+    public void removeBorrowedBook(Book book){
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString(){
+        return "[" + memberId + "]" + name + " (" + borrowedBooks.size() + " books borrowed)";
+    }
 }
